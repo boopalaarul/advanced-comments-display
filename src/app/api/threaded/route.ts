ac21253cd4bfeb1bed29f13d7b576636f8aac337
boopalaarul <boopalaarul@yahoo.com>
2023-12-27 20:22:02 -0800

    fixed error with a placeholder function left in api/threaded

--- src/app/api/threaded/route.ts
+++ src/app/api/threaded/route.ts
@@ -20,20 +20,3 @@ export async function GET(request: NextRequest) {
         throw new Error('Failed to fetch all comments.');
     }
 }
-
-
-export async function fetchRepliesTo(replying_to : number | null) {
-    // Add noStore() here prevent the response from being cached.
-    // This is equivalent to in fetch(..., {cache: 'no-store'}).
-  
-    try {
-        //fetch the replies to "replying_to" (if null, select the "root" comments)
-        const data = await sql<Comment>`SELECT * FROM revenue WHERE replying_to 
-            ${replying_to ? `= ${replying_to}` : "IS NULL"}`;
-
-      return data.rows;
-    } catch (error) {
-      console.error('Database Error:', error);
-      throw new Error(`Failed to fetch replies to ${replying_to ? replying_to : "root"}.`);
-    }
-  }
